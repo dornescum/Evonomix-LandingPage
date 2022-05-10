9254a35b516364390346fcd7fe6bda5e370b222b
Mihai Dornescu <dornescu@gmail.com>
2022-05-10 15:35:14 +0300

    jquery

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,78 @@
 import {slides} from "./utils.js";
 
-const toggleNav = document.querySelector('.toggle-nav');
-const sidebarOverlay = document.querySelector('.sidebar-overlay');
-const closeBtn = document.querySelector('.sidebar-close');
-const linkMenu = document.getElementById('link');
-const destinatiiInterne = document.getElementById('destinatii-interne');
-let lastEl = linkMenu.lastElementChild;
-const carousel = document.getElementById('carousel');
-const videoContainer = document.querySelector('.video');
-const videoBtn = document.querySelector('.btn-icon');
-const form = document.getElementById('form');
+const toggleNav = $('.toggle-nav');
+const sidebarOverlay = $('.sidebar-overlay');
+const closeBtn = $('.sidebar-close');
+// const linkMenu = document.getElementById('link');
+// const destinatiiInterne = document.getElementById('destinatii-interne');
+const destinatiiInterne = $('#destinatii-interne');
+const hiddenLinks = $('.hidden-links');
+
+
+// let lastEl = linkMenu.lastElementChild;
+const carousel = $('#carousel');
+const videoContainer = $('.video');
+const videoBtn = $('.btn-icon');
+const form = $('form');
 const imgCarousel = document.getElementById('carousel-image');
 
 
 // nav
-toggleNav.addEventListener('click', () => {
-	sidebarOverlay.classList.add('show');
-});
-closeBtn.addEventListener('click', () => {
-	sidebarOverlay.classList.remove('show');
-});
+toggleNav.on('click', () => sidebarOverlay.addClass('show'));
+closeBtn.on('click', () => sidebarOverlay.removeClass('show'));
 // =========
-destinatiiInterne.addEventListener('click', () => {
-	lastEl.classList.toggle('show-links');
+
+destinatiiInterne.on('click', function () {
+	hiddenLinks.toggleClass('show-links');
+
 });
-carousel.setAttribute('class', 'carousel');
+carousel.attr('class', 'carousel');
 
 // video
-videoBtn.addEventListener('click', () => {
-	const icon = videoBtn.firstElementChild;
-	if (icon.classList.contains("icon-play-circled")) {
-		icon.classList.remove('icon-play-circled');
-		icon.classList.add('icon-pause');
-		videoContainer.play();
-	} else if (icon.classList.contains("icon-pause")) {
-		icon.classList.remove('icon-pause');
-		icon.classList.add('icon-play-circled');
-		videoContainer.pause();
+
+videoBtn.on('click', () => {
+	const icon = $('.btn-icon').children();
+	if (icon.hasClass("icon-play-circled")) {
+		icon.removeClass('icon-play-circled');
+		icon.addClass('icon-pause');
+		videoContainer.get(0).play();
+	} else if (icon.hasClass("icon-pause")) {
+		icon.removeClass('icon-pause');
+		icon.addClass('icon-play-circled');
+		videoContainer.get(0).pause();
 	}
 });
 
 // form
-form.addEventListener('click', (e) => {
+form.on('click', (e) => {
 	e.preventDefault();
 	let allData = [];
-	let formDestinations = document.getElementById("form").elements[0].value;
-	let formBudget = document.getElementById("form").elements[1].value;
-	let formName = document.getElementById("form").elements[2].value;
-	let formEmail = document.getElementById("form").elements[3].value;
+	let formDestinations = $("#destinations").val();
+	let formBudget = $("#budget").val();
+	let formName = $("#fullName").val();
+	let formEmail = $("#email").val();
 	allData.push(formDestinations, formBudget, formName, formEmail);
 	console.log(allData);
-	form.reset();
+
+	const trip = `${formName} wants to go to ${formDestinations} and has ${formBudget} $`;
+	$('#alertInfo').text(trip).addClass('testing');
+	const dataKey = $('.bg-red').data('key1', 1234);
+	// console.log(dataKey);
+	setTimeout(() => {
+		// $('#alertInfo').addClass('hidden');
+		$('#alertInfo').remove();
+	}, 10000);
+	form.trigger('reset')
+	// form.reset();
 });
 
 
 imgCarousel.innerHTML = slides.map(item => {
-	const {background: {backgroundImage, backgroundColor}, title, description} = item;
+	const {background: {backgroundImage, backgroundColor}, title, description, id} = item;
 	if (title === "" || description === "" || backgroundImage === "") {
 		return `        	<div class="item" style=${backgroundColor}>
                 				<img src='https://images.unsplash.com/photo-1623018035782-b269248df916?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXJyb3J8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
-                   				  alt=${title}>
+                   				  alt=${title} datatype=${id}>
                    				  <article>
                    				  	   <h1>N/A</h1>
                    				   <p>N/A</p>
@@ -73,7 +86,7 @@ imgCarousel.innerHTML = slides.map(item => {
 	}
 	return `        	<div class="item">
                 				<img src=${backgroundImage}
-                   				  alt=${title}>
+                   				  alt=${title} datatype=${id}>
                    				  <article>
                    				  	   <h1>${title}</h1>
                    				   <p>${description}</p>
